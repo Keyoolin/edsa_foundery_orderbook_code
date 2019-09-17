84717449265d1aac8687fe8041172278eaa610f0
keyoolin <keyoolin.padayachee@gmail.com>
2019-09-17 09:47:05 +0200

    Fixed bugs in previous code

--- delta_trade_price.py
+++ delta_trade_price.py
@@ -2,14 +2,14 @@ def get_trade_price(snapshot_1, bid_column = 'bids_price', ask_column = "asks_pr
     return (snapshot_1[bid_column][0] + snapshot_1[ask_column][0])/2
     
 def get_trade_price_change(snapshot_1, snapshot_2, bid_column = 'bids_price', ask_column = "asks_price"):
-    snapshot_1_price = get_trade_price(snapshot_1, bid_column = 'bids_price', ask_column = "asks_price")
-    snapshot_2_price = get_trade_price(snapshot_2, bid_column = 'bids_price', ask_column = "asks_price")
+    snapshot_1_price = get_trade_price(snapshot_1, bid_column = bid_column, ask_column = ask_column)
+    snapshot_2_price = get_trade_price(snapshot_2, bid_column = bid_column, ask_column = ask_column)
     
     return snapshot_2_price - snapshot_1_price
     
     
 def trade_price_change_classifier(snapshot_1, snapshot_2, bid_column = 'bids_price', ask_column = "asks_price"):
-    change = get_trade_price_change(snapshot_1, snapshot_2, bid_column = 'bids_price', ask_column = "asks_price")
+    change = get_trade_price_change(snapshot_1, snapshot_2, bid_column = bid_column, ask_column = ask_column)
     
     if change > 0:
         return 1
@@ -27,7 +27,7 @@ def delta_trade_price_final_classifier(data, increment, bid_column = 'bids_price
         snapshot_2 = data[start+increment]
         delta_price.append(trade_price_change_classifier(snapshot_1,
                                                          snapshot_2,
-                                                         bid_column = 'bids_price',
-                                                         ask_column = "asks_price"))
+                                                         bid_column = bid_column,
+                                                         ask_column = ask_column))
         start = start + 1
     return delta_price
